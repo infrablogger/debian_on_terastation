08cfbbc8fbb41ab3aaec454061cb93178b606404
infrablogger <infrablogger1@gmail.com>
2022-01-06 09:33:44 +0900

    液晶ディスプレイにホスト名を表示

--- ansible/roles/TS5000/files/micon_scripts/micon_startup.py
+++ ansible/roles/TS5000/files/micon_scripts/micon_startup.py
@@ -2,82 +2,87 @@
 
 import libmicon
 import platform
+import socket
 
 
 ###make miconv2 and v3 start up tasks functions.
 
-def startupV2(port):
-	test = libmicon.micon_api(port)
+def get_title():
+	return "RemoteSupportKit"
 
-	micon_version = test.send_read_cmd(0x83)
+def get_hostname():
+	return socket.gethostname()
+
+def startupV2(port):
+	micon = libmicon.micon_api(port)
+
+	micon_version = micon.send_read_cmd(0x83)
 	micon_version=micon_version.decode('utf-8')
 
 	##disable boot watchdog
-	test.send_write_cmd(0,0x03)
+	micon.send_write_cmd(0,0x03)
 
-	title = "Buffalo".center(16)
-	version= "RemoteSupportKit".center(16)
+	title = get_title().center(16)
+	version= get_hostname().center(16)
 
 	### need to understand variations of this
 	##turn of red drive leds
-	test.cmd_set_led(libmicon.LED_OFF,[0x00,0x0F])
+	micon.cmd_set_led(libmicon.LED_OFF,[0x00,0x0F])
 
-	test.set_lcd_buffer(0x90,title,version)
-	test.cmd_force_lcd_disp(libmicon.lcd_disp_buffer0)
-	test.send_write_cmd(1,libmicon.lcd_set_dispitem,0x20)
-	test.set_lcd_brightness(libmicon.LCD_BRIGHT_FULL)
+	micon.set_lcd_buffer(0x90,title,version)
+	micon.cmd_force_lcd_disp(libmicon.lcd_disp_buffer0)
+	micon.send_write_cmd(1,libmicon.lcd_set_dispitem,0x20)
+	micon.set_lcd_brightness(libmicon.LCD_BRIGHT_FULL)
 
 	if (micon_version.find("HTGL") == -1):
-		test.set_lcd_color(libmicon.LCD_COLOR_GREEN)
+		micon.set_lcd_color(libmicon.LCD_COLOR_GREEN)
 
-	test.cmd_set_led(libmicon.LED_ON,libmicon.POWER_LED)
+	micon.cmd_set_led(libmicon.LED_ON,libmicon.POWER_LED)
 
-	test.port.close()
+	micon.port.close()
 
 def startupV3(port):
-	test = libmicon.micon_api_v3(port)
+	micon = libmicon.micon_api_v3(port)
 
 	##disable watchdog
-	test.send_miconv3("BOOT_END")
+	micon.send_miconv3("BOOT_END")
 
-	file = open("/etc/debian_version", "r")
-	version= "Debian " + file.readline().strip()
-	version = version.center(16,u"\u00A0")
-	title = "Terastation " + platform.machine()[:3].upper()
+	title = get_title().center(16,u"\u00A0")
+	version = get_hostname().center(16,u"\u00A0")
 
-	print(test.set_lcd(0,title))
-	print(test.set_lcd(1,version))
+	print(micon.set_lcd(0,title))
+	print(micon.set_lcd(1,version))
 
 	##solid power LED
-	test.set_led(0,"on")
-	test.port.close()
+	micon.set_led(0,"on")
+	micon.port.close()
 
 # check for some sort of config file to avoid messing with ports each time?
 
 ##try reading micon version from each port to determine the right one
 for port in ["/dev/ttyS1","/dev/ttyS3"]:
 	try:
-		test = libmicon.micon_api(port)
+		micon = libmicon.micon_api(port)
 	except:
 		continue
-	micon_version = test.send_read_cmd(0x83)
+	micon_version = micon.send_read_cmd(0x83)
 	if micon_version:
-		test.port.close()
+		micon.port.close()
 		startupV2(port)
 		quit()
-	test.port.close()
+	micon.port.close()
 
 
 for port in ["/dev/ttyUSB0","/dev/ttyS1","/dev/ttyS0"]:
 	try:
-		test = libmicon.micon_api_v3(port)
+		micon = libmicon.micon_api_v3(port)
 	except:
 		continue
-	micon_version = test.send_miconv3("VER_GET")
+	micon_version = micon.send_miconv3("VER_GET")
 	if micon_version:
-		test.port.close()
+		micon.port.close()
 		startupV3(port)
 		quit()
-	test.port.close()
+	micon.port.close()
 
 quit()
